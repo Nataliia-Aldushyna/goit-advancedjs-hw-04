1f50fd573cdf8b5a801612b4c5ece57e894197fc
Nataliia Aldushyna <nataliia.aldushyna@gmail.com>
2024-10-23 18:33:18 +0200

    update render-functions.js

---
+++ src/js/render-functions.js
@@ -0,0 +1,54 @@
+function getImagesMarkup(images) {
+    return images.hits
+      .map(
+        element =>
+          `<li class="gallery-item">
+      <a class="gallery-link" href="${element.largeImageURL}">
+          <img
+              class="gallery-image"
+              src="${element.webformatURL}"
+              alt="${element.tags}"
+              />
+      </a>
+    <ul class="image-info">
+     <li class="image-item-info">
+          <span class="text-accent">Likes</span>
+          <span class="text-comment">${element.likes}</span>
+      </li>
+      <li class="image-item-info">
+        <span class="text-accent">Views</span>
+        <span class="text-comment">${element.views}</span>
+      </li>
+      <li class="image-item-info">
+        <span class="text-accent">Comments</span>
+        <span class="text-comment">${element.comments}</span>
+      </li>
+      <li class="image-item-info">
+        <span class="text-accent">Downloads</span>
+        <span class="text-comment">${element.downloads}</span>
+      </li>
+    </ul>
+   
+  </li>`
+      )
+      .join('');
+  }
+  
+  function toggleLoadMoreButton(show, moreBtn, dataSearch = '') {
+    if (!show) {
+      moreBtn.classList.add('hide');
+      return;
+    }
+    moreBtn.classList.remove('hide');
+    moreBtn.dataset.search = dataSearch;
+  }
+  
+  function toggleSearchEndText(show, textEl) {
+    if (!show) {
+      textEl.classList.add('hide');
+      return;
+    }
+    textEl.classList.remove('hide');
+  }
+  
+  export { getImagesMarkup, toggleLoadMoreButton, toggleSearchEndText };
